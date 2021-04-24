4b8dc0e8c89234bccece3b2c00b3bd1085711d30
Masaki Hara <ackie.h.gmai@gmail.com>
2021-04-24 13:31:23 +0900

    Parse paren

--- src/parser.rs
+++ src/parser.rs
@@ -18,6 +18,16 @@ impl Parser {
                 self.pos += 1;
                 Ok(Prop::Atom(ident.clone()))
             }
+            Some(&Token::LParen) => {
+                self.pos += 1;
+                let prop = self.parse_prop()?;
+                if let Some(&Token::RParen) = self.tokens.get(self.pos) {
+                    self.pos += 1;
+                    Ok(prop)
+                } else {
+                    Err(ParseError)
+                }
+            }
             _ => Err(ParseError),
         }
     }
@@ -62,7 +72,7 @@ mod tests {
         use Prop::*;
 
         let prop = parse_prop("A").unwrap();
-        assert_eq!(prop, Atom(S("A")))
+        assert_eq!(prop, Atom(S("A")));
     }
 
     #[test]
@@ -70,7 +80,7 @@ mod tests {
         use Prop::*;
 
         let prop = parse_prop("foo23").unwrap();
-        assert_eq!(prop, Atom(S("foo23")))
+        assert_eq!(prop, Atom(S("foo23")));
     }
 
     #[test]
@@ -83,7 +93,7 @@ mod tests {
         use Prop::*;
 
         let prop = parse_prop("\nfoo23 ").unwrap();
-        assert_eq!(prop, Atom(S("foo23")))
+        assert_eq!(prop, Atom(S("foo23")));
     }
 
     #[test]
@@ -91,7 +101,7 @@ mod tests {
         use Prop::*;
 
         let prop = parse_prop("A ∧ B").unwrap();
-        assert_eq!(prop, Conj(vec![Atom(S("A")), Atom(S("B"))]))
+        assert_eq!(prop, Conj(vec![Atom(S("A")), Atom(S("B"))]));
     }
 
     #[test]
@@ -99,7 +109,7 @@ mod tests {
         use Prop::*;
 
         let prop = parse_prop("A ∧ B ∧ C").unwrap();
-        assert_eq!(prop, Conj(vec![Atom(S("A")), Atom(S("B")), Atom(S("C"))]))
+        assert_eq!(prop, Conj(vec![Atom(S("A")), Atom(S("B")), Atom(S("C"))]));
     }
 
     #[test]
@@ -110,6 +120,64 @@ mod tests {
     #[test]
     fn test_conj4() {
         parse_prop("A ∧ ∧ B").unwrap_err();
+    }
+
+    #[test]
+    fn test_paren1() {
+        use Prop::*;
+
+        let prop = parse_prop("(A)").unwrap();
+        assert_eq!(prop, Atom(S("A")));
+    }
+
+    #[test]
+    fn test_paren2() {
+        use Prop::*;
+
+        let prop = parse_prop("(A ∧ B)").unwrap();
+        assert_eq!(prop, Conj(vec![Atom(S("A")), Atom(S("B"))]));
+    }
+
+    #[test]
+    fn test_paren3() {
+        use Prop::*;
+
+        let prop = parse_prop("(A ∧ B) ∧ C").unwrap();
+        assert_eq!(
+            prop,
+            Conj(vec![Conj(vec![Atom(S("A")), Atom(S("B"))]), Atom(S("C"))])
+        );
+    }
+
+    #[test]
+    fn test_paren4() {
+        use Prop::*;
+
+        let prop = parse_prop("A ∧ (B ∧ C)").unwrap();
+        assert_eq!(
+            prop,
+            Conj(vec![Atom(S("A")), Conj(vec![Atom(S("B")), Atom(S("C"))])])
+        );
+    }
+
+    #[test]
+    fn test_paren5() {
+        parse_prop("(B").unwrap_err();
+    }
+
+    #[test]
+    fn test_paren6() {
+        parse_prop("B(").unwrap_err();
+    }
+
+    #[test]
+    fn test_paren7() {
+        parse_prop(")A").unwrap_err();
+    }
+
+    #[test]
+    fn test_paren8() {
+        parse_prop("A)").unwrap_err();
     }
 
     #[allow(non_snake_case)]
