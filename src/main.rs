a325b39409ad200a810c3bd1d87533851c2184d3
Masaki Hara <ackie.h.gmai@gmail.com>
2021-05-03 22:56:15 +0900

    Add provability tests

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ pub mod nj;
 pub mod parser;
 pub mod prop;
 pub mod rollback;
+#[cfg(test)]
+mod tests;
 pub mod tokenizer;
 
 fn main() {
---
+++ src/tests.rs
@@ -0,0 +1,57 @@
+// TODO: some of the functions should eventually be rearranged as public APIs.
+
+use crate::prop::Env;
+use crate::prop::{IdGen, Prop};
+use crate::{ipc::solve, parser::parse_prop};
+
+const PROPS: &[(&str, bool)] = &[
+    ("A", false),
+    ("A → A", true),
+    ("A → A → A", true),
+    ("(A → A) → A", false),
+    ("A → B", false),
+    ("A → B → A", true),
+    ("B → A → A", true),
+    ("((A → B) → A) → A", false),
+    ("A → B → A ∧ B", true),
+    ("A → A ∧ A", true),
+    ("(A → A) ∧ A", false),
+    ("A ∧ B → B ∧ A", true),
+    ("A ∧ B ∧ C → B ∧ C ∧ A", true),
+    ("(A ∧ B) ∧ C → A ∧ (B ∧ C)", true),
+    ("(A ∧ B) ∧ C → A ∧ B ∧ C", true),
+    ("A ∧ B ∧ C → B", true),
+    ("(A → B) ∧ (A → C) ∧ (B → C → D) → A → D", true),
+    ("⊤", true),
+    ("⊤ → A", false),
+    ("A ∨ (A → A)", true),
+    ("A ∨ A → A", true),
+    ("A ∨ B → B", false),
+    ("A → A ∨ B", true),
+    ("(A → B) ∨ (B → A)", false),
+    ("A ∨ B ∨ C → A ∨ B", false),
+    ("⊥", false),
+    ("⊥ → A", true),
+    ("A ∨ (A → ⊥)", false),
+    ("A ∨ ⊥ → A", true),
+    ("⊤ → ⊥", false),
+    ("⊥ → ⊤", true),
+    ("(A → A → ⊥) ∧ ((A → ⊥) → A) → ⊥", true),
+    ("((A ∨ (A → ⊥)) → ⊥) → ⊥", true),
+    ("((((A → B) → A) → A) → ⊥) → ⊥", true),
+    ("(A ∨ (A → ⊥)) → ((A → ⊥) → ⊥) → A", true),
+    ("(A ∧ B) ∨ C → (A ∨ C) ∧ (B ∨ C)", true),
+    ("(A ∨ C) ∧ (B ∨ C) → (A ∧ B) ∨ C", true),
+];
+
+#[test]
+fn test_provability() {
+    for &(prop_str, provable_expect) in PROPS {
+        let mut idgen = IdGen::new();
+        let mut env = Env::new();
+        let ast = parse_prop(prop_str).unwrap();
+        let prop = Prop::from_ast(&mut idgen, &mut env, &ast);
+        let provable = solve(&prop).is_some();
+        assert_eq!(provable, provable_expect, "{}", prop_str);
+    }
+}
